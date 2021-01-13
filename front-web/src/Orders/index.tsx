a8ce77082b272e4897bf9d0a2c07850f4ccba26a
Pedro Ferreira <pedro.ferreirasouza@gmail.com>
2021-01-13 09:16:41 -0500

    Add routes and Steps

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,13 @@
+import StepsHeader from "./StepsHeader";
+import "./styles.css";
+// import { ReactComponent as Logo } from "./logo.svg";
+
+function Orders() {
+  return (
+    <div className="orders-container">
+        <StepsHeader/>
+    </div>
+  );
+}
+
+export default Orders;
